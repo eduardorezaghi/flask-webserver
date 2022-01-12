3f34262831ca785045cd3d1a720800e8e22ff3ba
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2022-01-12 16:52:41 -0300

    Add POST routes
    Added routes to parse POST requests, as follows:
    - POST route to add new items in a shopping cart, which can be from HTML
    form or JSON (from curl or another method).

--- app.py
+++ app.py
@@ -48,6 +48,51 @@ def get_carrinho_item(id):
     return make_response(jsonify({"Item": item[0]}), 200)
 # ----------------------------------------------------------------------------
 
+# ROTAS POST
+# ----------------------------------------------------------------------------
+
+# Retorna a atualização do carrinho com o novo item adicionado por POST
+@app.route('/post/', methods=["GET","POST"])
+def post_form():
+    if request.method == 'GET':
+        # Renderiza um formulário para inserção de dados
+        return render_template('form.html')
+    
+    # Se a requisição vir de um formulário
+    if request.form:
+
+        # Extrai o valor de "nome" na requisição do formulário
+        nome = request.form['nome']
+        
+        # Se o valor de nome for um valor inválido
+        if not nome:
+            return make_response(jsonify({"erro":"Item invalido"}), 404)
+
+        # Criação de um novo item
+        novo_item = {
+            # Incremento do id: valor do ID do último item do carrinho
+            # somado com 1
+            'id': carrinho[-1]['id'] + 1,
+            # Nome extraído da requisição
+            'nome': nome
+        }
+
+        # Insere no final do carrinho o novo item criado
+        carrinho.append(novo_item)
+
+    # Se a requisição vir por meio de um JSON
+    # ex: >> curl -d "nome=cd player" -X POST http://127.0.0.1:5000/post/
+    else:
+        # Extrai o valor de "nome" na requisição do JSON
+        nome = request.json['nome']
+        
+        # Se o valor de nome for um valor inválido
+        if not nome:
+            return make_response(jsonify({"erro":"Item invalido"}), 404)
+
+    # Exibe novamente todos os itens no carrinho de compras
+    return make_response(jsonify(carrinho), 200)
+# ----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run(debug=True)
